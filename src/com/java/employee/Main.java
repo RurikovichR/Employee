eda3e13da5154963efe73aa468f33a344e870293
RurikovichR <51745436+RurikovichR@users.noreply.github.com>
2020-03-22 19:40:39 +0300

    Compl

---
+++ src/com/java/employee/Main.java
@@ -0,0 +1,9 @@
+package com.java.employee;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Ivan", "Engener", 10000);
+        System.out.println(employee.getInfo(12));
+
+    }
+}
